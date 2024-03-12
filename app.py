ce8531f4ff9c929c38089f8672ca2bb013cadade
Ayxan <70232602+cavablar@users.noreply.github.com>
2024-03-12 13:07:26 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/save_message', methods=['POST'])
+def save_message():
+    message = request.form['message']
+    with open('message.txt', 'a') as f:
+        f.write(message)
+    return 'Message saved successfully!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
